14386fb21d412b6b56ecfa4ced8a69914e975f84
barbara-musial <basia.musial22@op.pl>
2021-11-17 01:10:07 +0100

    Refactor code and adding delete task function

--- script.js
+++ script.js
@@ -11,10 +11,13 @@ addTaskButton.addEventListener("click", function () {
   const html = `
   <div id="n${tasks.length}">
     <div id="task${tasks.length}">
-      <input type="checkbox" id="taskCheck${tasks.length}" onclick="checkboxChecked(${tasks.length})"><label id="taskContent${tasks.length}">${usersInput.value}</label><input type="button" id='renameButton${tasks.length}' onclick='renameTask(${tasks.length})' value="Rename"><br>
+      <input type="checkbox" id="taskCheck${tasks.length}" onclick="checkboxChecked(${tasks.length})">
+      <label id="taskContent${tasks.length}">${usersInput.value}</label>
+      <input type="button" id='renameButton${tasks.length}' onclick='renameTask(${tasks.length})' value="Rename">
+      <input type="button" id='deleteButton${tasks.length}' onclick="deleteTask(${tasks.length})" value="X" style="display: none">
     </div>
     <div id="taskRename${tasks.length}" style="display: none">
-      <input type="text" id="renamedTask${tasks.length}" value="${usersInput.value}"><input type="button" onclick="submitTaskRename(${tasks.length})" value="✔"><br>
+      <input type="text" id="renamedTask${tasks.length}" value="${usersInput.value}"><input type="button" onclick="submitTaskRename(${tasks.length})" value="✔">
     </div>
   </div>`;
 
@@ -43,24 +46,25 @@ const submitTaskRename = function (taskNumber) {
 };
 
 const checkboxChecked = function (taskNumber) {
-  const task = document.getElementById(`task${taskNumber}`);
   const taskContent = document.getElementById(`taskContent${taskNumber}`);
   const checkbox = document.getElementById(`taskCheck${taskNumber}`);
   const deleteButton = document.getElementById(`deleteButton${taskNumber}`);
+  const renameButton = document.getElementById(`renameButton${taskNumber}`);
 
   if (checkbox.checked) {
     taskContent.style.color = "red";
-    task.removeChild(renameButton);
-    const html = `
-    <input type="button" id='deleteButton${taskNumber}' value="X">
-    `;
-    taskContent.insertAdjacentHTML("afterend", html);
+
+    deleteButton.style.display = "block";
+    renameButton.style.display = "none";
   } else {
     taskContent.style.color = "black";
-    const html = `
-    <input type="button" id='renameButton${taskNumber}' onclick='renameTask(${taskNumber})' value="Rename">
-    `;
-    task.removeChild(deleteButton);
-    taskContent.insertAdjacentHTML("afterend", html);
+
+    deleteButton.style.display = "none";
+    renameButton.style.display = "block";
   }
+};
+
+const deleteTask = function (taskNumber) {
+  const taskToDelete = document.getElementById(`n${taskNumber}`);
+  tasksList.removeChild(taskToDelete);
 };
